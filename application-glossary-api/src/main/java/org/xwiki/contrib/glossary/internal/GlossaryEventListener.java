e7865b57d1a6381af5dc281178e3b965cd5f51bc
Clément Aubin <aubincleme@gmail.com>
2021-10-08 14:02:47 +0200

    GLOSSARY-24 : Add ability to translate glossary entries
    * Update the GlossaryEventListener to update the cache when the title of
      an entry is updated, and when a translated entry is created / deleted

--- application-glossary-api/src/main/java/org/xwiki/contrib/glossary/internal/GlossaryEventListener.java
+++ application-glossary-api/src/main/java/org/xwiki/contrib/glossary/internal/GlossaryEventListener.java
@@ -21,14 +21,17 @@ package org.xwiki.contrib.glossary.internal;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 
+import org.slf4j.Logger;
 import org.xwiki.bridge.event.DocumentCreatedEvent;
 import org.xwiki.bridge.event.DocumentDeletedEvent;
+import org.xwiki.bridge.event.DocumentUpdatedEvent;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.contrib.glossary.GlossaryCache;
 import org.xwiki.contrib.glossary.GlossaryModel;
@@ -38,7 +41,10 @@ import org.xwiki.model.reference.EntityReference;
 import org.xwiki.observation.EventListener;
 import org.xwiki.observation.event.Event;
 
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 
 /**
  * Update the Glossary cache when documents containing {@code Glossary.Code.GlossaryClass} xobjects are modified.
@@ -50,7 +56,8 @@ import com.xpn.xwiki.doc.XWikiDocument;
 @Singleton
 public class GlossaryEventListener implements EventListener
 {
-    private static final List<Event> EVENTS = Arrays.asList(new DocumentCreatedEvent(), new DocumentDeletedEvent());
+    private static final List<Event> EVENTS = Arrays.asList(new DocumentCreatedEvent(),
+        new DocumentUpdatedEvent(), new DocumentDeletedEvent());
 
     private static final EntityReference GLOSSARY_XCLASS_REFERENCE =
         new EntityReference("GlossaryClass", EntityType.DOCUMENT,
@@ -58,10 +65,13 @@ public class GlossaryEventListener implements EventListener
                 new EntityReference("Glossary", EntityType.SPACE)));
 
     @Inject
-    private Provider<GlossaryCache> cache;
+    private Provider<GlossaryCache> cacheProvider;
 
     @Inject
     private GlossaryModel glossaryModel;
+
+    @Inject
+    private Logger logger;
 
     @Override
     public List<Event> getEvents()
@@ -79,22 +89,79 @@ public class GlossaryEventListener implements EventListener
     public void onEvent(Event event, Object source, Object data)
     {
         XWikiDocument document = (XWikiDocument) source;
-        // Note:
-        // - if the doc is deleted then document.getOriginalDocument() is the doc before the deletion and thus
-        //   containing the xclass
-        // - if the doc is created then document.getOriginalDocument() is the same as document
-        if ((event instanceof DocumentCreatedEvent && document.getXObject(GLOSSARY_XCLASS_REFERENCE) != null)
-            || (event instanceof DocumentDeletedEvent
-                && document.getOriginalDocument().getXObject(GLOSSARY_XCLASS_REFERENCE) != null))
-        {
-            DocumentReference glossaryDocumentReference = document.getDocumentReferenceWithLocale();
-            String glossaryName = document.getTitle();
-            if (event instanceof DocumentCreatedEvent) {
-                this.cache.get().set(glossaryName, glossaryDocumentReference);
-            } else if (event instanceof DocumentDeletedEvent) {
-                this.cache.get().remove(glossaryName, document.getLocale(),
-                    glossaryModel.getGlossaryId(glossaryDocumentReference));
+        XWikiContext xWikiContext = (XWikiContext) data;
+
+        try {
+            // We'll need to decide based on the current document status if it is a glossary entry or not. For each
+            // supported event, the event can target either a document in its default language, or a translation of
+            // the document.
+
+            // Check if a document with the same document reference exists in the XWiki database with a glossary object
+            // This can return a non-null object in 3 cases :
+            // * We are working on an "original" (non-translated document), and we are just re-loading the same
+            // document as we have in parameter of the event
+            // * We are working with a translation of the document, and we are loading the non-translated version of
+            // this document
+            // * We are working with a translation document that just got deleted, however the original document
+            // remains in the database.
+            BaseObject glossaryObject = xWikiContext.getWiki().getDocument(document.getDocumentReference(),
+                xWikiContext).getXObject(GLOSSARY_XCLASS_REFERENCE);
+            boolean hasObject = glossaryObject != null;
+            boolean isCreateOrUpdate = (event instanceof DocumentCreatedEvent || event instanceof DocumentUpdatedEvent);
+            boolean hasOrHadObject =
+                (hasObject || document.getOriginalDocument().getXObject(GLOSSARY_XCLASS_REFERENCE) != null);
+
+            // Note: if the original doc is deleted then document.getOriginalDocument() is the doc before the
+            // deletion and thus containing the xclass
+            if ((isCreateOrUpdate && hasObject) || (event instanceof DocumentDeletedEvent && hasOrHadObject)) {
+                handleEvent(document, event);
             }
+
+        } catch (XWikiException e) {
+            logger.error("Failed to update the glossary cache.", e);
         }
+    }
+
+    private void handleEvent(XWikiDocument document, Event event)
+    {
+        DocumentReference glossaryDocumentReference;
+        String glossaryTitle;
+        GlossaryCache cache = this.cacheProvider.get();
+
+        if (event instanceof DocumentCreatedEvent) {
+            glossaryDocumentReference = getLocalizedDocumentReference(document);
+            glossaryTitle = document.getTitle();
+
+            cache.set(glossaryTitle, glossaryDocumentReference);
+        } else if (event instanceof DocumentUpdatedEvent) {
+            glossaryDocumentReference = getLocalizedDocumentReference(document);
+            glossaryTitle = document.getTitle();
+
+            String glossaryOldTitle = document.getOriginalDocument().getTitle();
+            String glossaryId = glossaryModel.getGlossaryId(glossaryDocumentReference);
+
+            cache.remove(glossaryOldTitle, glossaryDocumentReference.getLocale(), glossaryId);
+            cache.set(glossaryTitle, glossaryDocumentReference);
+
+        } else if (event instanceof DocumentDeletedEvent) {
+            glossaryDocumentReference = getLocalizedDocumentReference(document.getOriginalDocument());
+            glossaryTitle = document.getOriginalDocument().getTitle();
+
+            cache.remove(glossaryTitle, glossaryDocumentReference.getLocale(),
+                glossaryModel.getGlossaryId(glossaryDocumentReference));
+        }
+    }
+
+    private DocumentReference getLocalizedDocumentReference(XWikiDocument document)
+    {
+        DocumentReference glossaryDocumentReference = document.getDocumentReferenceWithLocale();
+
+        // We want to avoid storing glossary entries with empty locales, as the XWiki Context will never return
+        // an empty locale, which will prevent us afterwards to properly search for glossary entries in the cache.
+        if (glossaryDocumentReference.getLocale().equals(Locale.ROOT)) {
+            return new DocumentReference(glossaryDocumentReference, document.getDefaultLocale());
+        }
+
+        return glossaryDocumentReference;
     }
 }
