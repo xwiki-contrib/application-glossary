48aad8f05b7e72162f14ba349fb45c1c6b0b8f9c
Vincent Massol <vincent@massol.net>
2022-05-05 11:46:53 +0200

    GLOSSARY-37: Add support for Glossary PDF export when using the Process strategy * Split into 2 modules so that the process one is not installed by default (it's not needed by default since the default is to use docker)

--- application-glossary-latex/application-glossary-latex-pdf-process/src/main/java/org/xwiki/contrib/glossary/internal/latex/pdf/process/GlossaryLaTeX2PDFConfiguration.java
+++ application-glossary-latex/application-glossary-latex-pdf-process/src/main/java/org/xwiki/contrib/glossary/internal/latex/pdf/process/GlossaryLaTeX2PDFConfiguration.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.contrib.glossary.internal.process;
+package org.xwiki.contrib.glossary.internal.latex.pdf.process;
 
 import java.util.Arrays;
 import java.util.List;
